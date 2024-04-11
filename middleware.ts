89a42c992e7eb58d534199554c951e68d6b50f70
Lasya Yakkala <yakkala.lasya@heb.com>
2024-04-11 03:30:06 -0500

    allows continued authentication

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,15 @@
 //This file helps enable authentication and is where we configure protected routes for admin page
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
-  publicRoutes: ["/api/:path*"]
+  publicRoutes: ["/api/:path*"],
+  afterAuth(auth) {
+    if (auth.userId && !auth.isPublicRoute) {
+        return NextResponse.next();
+    }
+  }
+
 });
 
 export const config = {
